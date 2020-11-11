d9a0e376136600046f9e563637de1499e86b1dc9
godffather <pmrright@outlook.com>
2020-11-11 16:44:23 +0800

    自动构建可执行jar包的Windows脚本

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+echo 正在生成项目可执行jar包……
+del /Q out
+javac -cp src;mssql-jdbc-7.4.1.jre8.jar;mysql-connector-java-8.0.21.jar;selenium-server-standalone-3.141.59.jar src/RoutineCalculate/Main.java -d jar-temp -encoding utf8
+cd jar-temp
+jar -xvf ../mssql-jdbc-7.4.1.jre8.jar com microsoft mssql
+jar -xvf ../mysql-connector-java-8.0.21.jar com
+jar -xvf ../selenium-server-standalone-3.141.59.jar com javax net okhttp3 okio org
+jar -cvmf ../META-INF/MANIFEST.MF ../out/release.jar  ./*
+del /Q *
+cd ..
