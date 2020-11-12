1c73442ca54017f61961739862e1d1c0ccd803bf
mrright_zhao <pmrright@outlook.com>
2020-11-12 22:51:58 +0800

    Unix&Linux自动打包脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Unix&Linux自动编译打包脚本
 rm -rf ./jar-temp/*
 cd ./jar-temp
 jar -xvf ../mssql-jdbc-7.4.1.jre8.jar com microsoft mssql
 jar -xvf ../mysql-connector-java-8.0.21.jar com
 jar -xvf ../selenium-server-standalone-3.141.59.jar com javax net okhttp3 okio org
-javac -cp ../src:../mssql-jdbc-7.4.1.jre8.jar:../mysql-connector-java-8.0.21.jar:../selenium-server-standalone-3.141.59.jar ../src/RoutineCalculate.Main.java -d ./
+javac -cp ../src:../mssql-jdbc-7.4.1.jre8.jar:../mysql-connector-java-8.0.21.jar:../selenium-server-standalone-3.141.59.jar ../src/RoutineCalculate/Main.java -d ./ -encoding utf8
+jar -cvmf ../META-INF/MANIFEST.MF ../out/release.jar ./
+cd ..
+rm -rf ./jar-temp/*
