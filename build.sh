e9c2f0d08bcf5bd784d4375417cde85c2cf64c07
godfather <pmrright@outlook.com>
2021-01-30 19:17:07 +0800

    修复了MacOS&Linux的构建脚本

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# Unix&Linux自动编译打包脚本
-rm -rf ./jar-temp/*
-cd ./jar-temp
-jar -xvf ../mssql-jdbc-7.4.1.jre8.jar com microsoft mssql
-jar -xvf ../mysql-connector-java-8.0.21.jar com
-jar -xvf ../selenium-server-standalone-3.141.59.jar com javax net okhttp3 okio org
-jar -xvf ../gson-2.8.0.jar com
-javac -cp ../src:../mssql-jdbc-7.4.1.jre8.jar:../gson-2.8.0.jar:../mysql-connector-java-8.0.21.jar:../selenium-server-standalone-3.141.59.jar ../src/RoutineCalculate/Main.java -d ./ -encoding utf8
-jar -cvmf ../META-INF/MANIFEST.MF ../out/release.jar ./
+# Unix&Linux
+echo "正在构建项目执行jar包"
+javac -cp src:mssql-jdbc-7.4.1.jre8.jar:gson-2.8.0.jar:mysql-connector-java-8.0.21.jar:selenium-server-standalone-3.141.59.jar src/RoutineCalculate/Main.java -d jar-temp -encoding utf8
+cd jar-temp
+jar -cmf ../META-INF/MANIFEST.MF ../out/release.jar ./*
 cd ..
-rm -rf ./jar-temp
--- gen.sh
+++ gen.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # Unix/Linux解压依赖包到classpath
+echo 正在重新解压依赖jar包到classpath
+rm -rf out/*
+rm -rf jar-temp/*
+mkdir jar-temp
+cd jar-temp
+jar -xf ../mssql-jdbc-7.4.1.jre8.jar com microsoft mssql
+jar -xf ../mysql-connector-java-8.0.21.jar com
+jar -xf ../selenium-server-standalone-3.141.59.jar com javax net okhttp3 okio org
+jar -xf ../gson-2.8.0.jar com
+cd ..
